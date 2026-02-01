98eb6f8eb1593d259d4dec26034569485f1baa20
Khalil Hamani <hamani_khalil@yahoo.fr>
2026-02-01 12:44:50 +0100

    Bump version 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 group = "com.n-apos"
 
 description = "Simple gradle plugin to automate project version management"
-version = "0.1.0"
+version = "0.2.0"
 
 repositories {
     mavenCentral()
